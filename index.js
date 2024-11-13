177d659fcddd58f10f596a6b0dd173c1ce9f1fb6
Maximillian Atherton <max@a319.co.uk>
2024-11-13 19:16:50 +0000

    Attempt 1

--- index.js
+++ index.js
@@ -20,8 +20,16 @@ app.get('/', function (req, res) {
 });
 
 // your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+app.get('/api/whoami', function (req, res) {
+  ipString = req.ip
+  languageString = req.headers['accept-language']
+  softwareString = req.headers['user-agent']
+
+  res.json({ 
+    ipaddress: ipString,
+    language: languageString,
+    software: softwareString
+  });
 });
 
 // listen for requests :)
